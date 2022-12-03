b27ccd9f33136c3217d5b74c0383f2ef655a96cd
Chandra Shekhar <chandra.pandey@winning.com.au>
2022-12-03 14:11:47 +1100

    Recipe and ingredient Modal added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.modal';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
+
 export class RecipeListComponent implements OnInit {
+  
+  recipes: Recipe[] = [
+    new Recipe("Recipe 1","test description 1", 'https://www.shutterstock.com/image-photo/notepad-your-recipe-herbs-spices-260nw-370298699.jpg'),
+    new Recipe("Recipe 2","test description 2", 'https://www.shutterstock.com/image-photo/notepad-your-recipe-herbs-spices-260nw-370298699.jpg')
+  ]
 
   constructor() { }
 
