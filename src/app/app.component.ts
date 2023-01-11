e697910624d5ecb1b7dad09c22a912b4abca61be
Chandra Shekhar <chandra.pandey@winning.com.au>
2023-01-11 12:52:27 +1100

    Deep dive - structural directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,22 @@
+// import { Component, ElementRef } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.scss']
+// })
+// export class AppComponent {
+//   title = 'AngularRecepieBook';
+//   activeState = 'recipe';
+
+
+//   getCurrentState($event:string){
+//     this.activeState = $event;
+//     console.log(this.activeState);
+//   }
+// }
+
+
 import { Component, ElementRef } from '@angular/core';
 
 @Component({
@@ -6,12 +25,11 @@ import { Component, ElementRef } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AngularRecepieBook';
-  activeState = 'recipe';
+  numbers = [1,2,3,4,5];
+  oddNumbers = [1,3,5]
+  evenNumbers = [2,4,6]
+  onlyOdd = false
 
+  
 
-  getCurrentState($event:string){
-    this.activeState = $event;
-    console.log(this.activeState);
-  }
 }
