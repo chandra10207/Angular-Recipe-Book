8da35319c430b16aa75cdb240df258c43c7ae6a6
Chandra Shekhar <chandra.pandey@winning.com.au>
2023-01-21 16:07:58 +1100

    Fetching Route Parameters

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import {UsersComponent} from "./routing/users/users.component";
 import {HomeComponent} from "./routing/home/home.component";
 import {ServersComponent} from "./routing/servers/servers.component";
 import {RecipeBookComponent} from "./recipe-book/recipe-book.component";
+import {UserComponent} from "./routing/users/user/user.component";
 
 const routes: Routes = [
   {path: '', component: RecipeBookComponent},
   {path: 'routes', component: HomeComponent},
   {path: 'users', component: UsersComponent},
+  {path: 'users/:id/:name', component: UserComponent},
   {path: 'servers', component:ServersComponent}
 ];
 
